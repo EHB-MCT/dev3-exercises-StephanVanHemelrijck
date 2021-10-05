75117b371bb850a5ab66783ae8d515923fd70e94
Stephan Van Hemelrijck <Stephan.van.hemelrijck@student.ehb.be>
2021-10-05 11:35:11 +0200

    Hello world advanced
    Greet 5 classmates with the use of an array

--- session1/hello-world/src/main/kotlin/main.kt
+++ session1/hello-world/src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
 fun main(){
-    println("Hello World!")
+    val classmates: Array<String> = arrayOf("Sam", "Sascha", "Rebecca", "Stephan", "Jens")
+
+    for(classmate in classmates){
+        println(greetingClassmates(classmate))
+    }
+
+}
+
+fun greetingClassmates(classmate: String): String{
+    return "Hello $classmate"
 }
